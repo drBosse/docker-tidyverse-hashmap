283cd911fc168ca0372970062f5cc0647ce8a69d
Bosse Nyström <bon@praqma.net>
2019-10-17 21:36:48 +0200

    Uses correct tag

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 name=tidyverse-hashmap
 version=$(git describe --dirty)
 
-docker build -t $name:$version -t $name:latest . 
-#docker push $name:$version
-#docker push $name:latest
+docker build -t drbosse/$name:$version -t drbosse/$name:latest . 
+docker push drbosse/$name:$version
+docker push drbosse/$name:latest
