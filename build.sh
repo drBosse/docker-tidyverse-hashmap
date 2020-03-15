cdfba1cc651ecab41c9364f18217c0ab48da7b6d
Bosse Nyström <bon@praqma.net>
2020-03-15 20:24:24 +0100

    Add publish #1

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ "$1" == "Release" ]; then
+  release="TRUE"
+fi
+
 name=tidyverse-hashmap
 version=$(git describe --dirty)
 
 docker build -t drbosse/$name:$version -t drbosse/$name:latest .
 
-echo $CIRCLE_BRANCH
-#docker push drbosse/$name:$version
-#docker push drbosse/$name:latest
+if [ "$CIRCLE_BRANCH" == "master" ] || [ "$release" == "TRUE" ]; then
+  docker login -u $DOCKER_USER -p $DOCKER_TOKEN
+  docker push drbosse/$name:$version
+  docker push drbosse/$name:latest
+fi
