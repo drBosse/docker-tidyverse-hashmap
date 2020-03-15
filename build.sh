0c7975f911820ec08ed8a8ed4fd8c3c6813f5bd7
Bosse Nyström <bon@praqma.net>
2020-03-15 08:16:34 +0100

    WiP #1

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 name=tidyverse-hashmap
 version=$(git describe --dirty)
 
-docker build -t drbosse/$name:$version -t drbosse/$name:latest . 
-docker push drbosse/$name:$version
-docker push drbosse/$name:latest
+docker build -t drbosse/$name:$version -t drbosse/$name:latest .
+
+echo $CIRCLE_BRANCH
+#docker push drbosse/$name:$version
+#docker push drbosse/$name:latest
